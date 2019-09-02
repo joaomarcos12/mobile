a6c33023608241b5b6b4b154b3f5cc32a77ac616
Diego <diego.honshu@hotmail.com>
2019-09-02 20:53:58 -0300

    Segundo commit, adição de tela de login

--- app/src/main/java/br/com/etec/aula5/MainActivity.kt
+++ app/src/main/java/br/com/etec/aula5/MainActivity.kt
@@ -1,5 +1,6 @@
 package br.com.etec.aula5
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivityForResult(intent,1010)
+
     }
 }
